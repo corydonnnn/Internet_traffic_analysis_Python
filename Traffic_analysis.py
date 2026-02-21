f8d9077d156248fe4e3cf746a3314d4629cb465a
corydonnnn <adjynysalieva@gmail.com>
2026-02-22 00:55:00 +0800

    Update Traffic_analysis.py

--- Traffic_analysis.py
+++ Traffic_analysis.py
@@ -6,28 +6,28 @@ print(df.head(), '\n')
 print(df.tail(), '\n')
 print(df.shape, '\n')
 print(df.info(), '\n')
-print(df.describe(), '\n') #проверка типов
+print(df.describe(), '\n') 
 
-#проверка на наличие пустых строк и превращение их в 0
+#converting to 0
 print(df.isnull().sum(), '\n')
-#удаление сломанных строк
+#removing corrupt rows
 df = df.dropna(subset=['user_id', 'tariff_plan', 'region'])
 
-#преобразование типов
+#converting data
 df['plan_limit_gb'] = pd.to_numeric(df['plan_limit_gb'], errors='coerce')
 df['traffic_gb_used'] = pd.to_numeric(df['traffic_gb_used'], errors='coerce')
 
 df['traffic_gb_used'] = df['traffic_gb_used'].fillna(0)
 print(df.isnull().sum(), '\n')
-#создаем столбец over_limit
+#creating new column over_limit
 df['over_limit'] = df['traffic_gb_used'] > df['plan_limit_gb']
 print(df.head())
 
-report1 = df.groupby('region').size().rename('records_count').reset_index() #группировка
-report1 = report1.sort_values('records_count', ascending=False) #сортировка по убыванию
+report1 = df.groupby('region').size().rename('records_count').reset_index() #group
+report1 = report1.sort_values('records_count', ascending=False) #sorting values
 print("-" * 50)
-print("--- Отчет 1: Подсчет абонентов по регионам --- \n")
-print(report1.to_string(index=False)) #красивое оформление
+print("--- Report 1: Number of Subscribers by Region --- \n")
+print(report1.to_string(index=False)) 
 print("-" * 50)
 
 target_regions = ['Bishkek', 'Osh']
@@ -35,7 +35,7 @@ df_r2 = df[df['region'].isin(target_regions)].copy()
 report2 = df_r2.groupby('tariff_plan', as_index=False)['traffic_gb_used'].mean().rename(columns={'traffic_gb_used':'avg_traffic_gb_used'})
 report2 = report2.sort_values('avg_traffic_gb_used', ascending=False).reset_index(drop=True)
 
-print("--- Отчет 2: Абоненты из целевых регионов --- \n")
+print("--- Report 2: Subscribers from Target Regions --- \n")
 print(report2.to_string(index=False))
 print("-" * 50)
 
@@ -44,7 +44,7 @@ report3 = df[
     (df['tariff_plan'] == 'Standard') &
     (df['over_limit'] == True)][['user_id', 'traffic_gb_used', 'month']]
 
-print("--- Отчет 3: Фильтр проблемных пользователей --- \n")
+print("--- Report 3: Filter of Problematic Users --- \n")
 print(report3.to_string(index=False))
 print("-" * 50)
 
@@ -54,7 +54,7 @@ report4 = df.groupby('tariff_plan').agg(
     unique_users = ('user_id', 'nunique')
 ).reset_index()
 
-print("--- Отчёт 4: Общая сводка по тарифным планам --- \n")
+print("--- Report 4: Overall Summary by Tariff Plans --- \n")
 print(report4.to_string(index=False))
 print("-" * 50)
 
@@ -62,7 +62,7 @@ report5 = (df.groupby('region')['traffic_gb_used'].mean()
            .sort_values(ascending=False).head(5).reset_index()
            .rename(columns={'traffic_gb_used': 'avg_traffic_gb_used'}))
 
-print(" --- Отчёт 5: Топ-5 регионов по среднему трафику --- \n")
+print(" --- Report 5: Top 5 Regions by Average Traffic --- \n")
 print(report5.to_string(index=False))
 print("-" * 50)
 
@@ -71,7 +71,7 @@ report6 = df.groupby('tariff_plan').agg(
     max_traffic_gb=('traffic_gb_used', 'max')
 ).reset_index()
 
-print("--- Отчёт 6: Экстремумы трафика по тарифам --- \n")
+print("--- Report 6: Traffic Extremes by Tariff Plans --- \n")
 print(report6.to_string(index=False))
 print("-" * 50)
 
@@ -82,7 +82,7 @@ report7 = df_r7.groupby('tariff_plan').agg(
 ).reset_index()
 report7['percent_overlimit'] = (report7['overlimit_users'] / report7['total_users']) * 100
 
-print(" --- Отчет 7: Анализ превышения лимитов --- \n ")
+print(" --- Report 7: Analysis of Limit Exceedances --- \n ")
 print(report7.to_string(index=False))
 print("-" * 50)
 
@@ -94,18 +94,11 @@ report8 = pd.pivot_table(
     aggfunc='sum',
     fill_value=0
 )
-print("--- Отчёт 8: Динамика трафика по месяцам --- \n")
+print("--- Report 8: Monthly Traffic Dynamics --- \n")
 print(report8)
 print("-" * 50)
 
-print('-Какой регион имеет больше всего абонентов (см. Отчет 1)?')
-print('Судя по результатам первого отчета больше всего абонентов в регионе "Ош"')
-print('-Какой тарифный план самый "проблемный" с точки зрения процента превышения лимитов (см. Отчет 7)?')
-print('Cамым проблемным тарифным планом считается "Standard". Процент превышения лимита составил 27.216495')
-print('-Какое максимальное потребление было на тарифе "Unlimited" (см. Отчет 6)?')
-print('На тарифе Unlimited максимальное потребление трафика составило 116.79')
-print('-Наблюдается ли рост или падение потребления трафика от января к марту (см.Отчет 8)?')
-print('По тарифу "Premium" наблюдается рост трафика, в то время как у тарифов "Standard" и "Unlimited" падение')
+
 
 
 
